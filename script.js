63ca8f31c000bbb420fb691a9b8db3220a171acf
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-10 19:10:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let firstName = document.getElementById('firstname')
+let lastName = document.getElementById('lastname')
+let mobile = document.getElementById('mobile')
+let mail = document.getElementById('mail')
+
+let form = document.getElementById('form')
+
+form.addEventListener('submit',()=>{
+	
+alert(`
+First Name : ${firstName.value}
+Last Name : ${lastName.value}
+Phone Number :${mobile.value}
+Email ID : ${mail.value}`
+	 )})
